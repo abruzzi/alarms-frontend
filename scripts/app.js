04d5dffadda4389441e554f9bcbb7fdc4f5cb454
Qiu Juntao <juntao.qiu@gmail.com>
2015-02-22 15:47:41 +1100

    refactor the code

--- scripts/app.js
+++ scripts/app.js
@@ -1,18 +1,29 @@
 $(function() {
-	$.get("/alarms.json").done(function(alarms) {
-		var eventCompiled = _.template($("#events").html());
+	function update(container, key, data) {
+		var compiled = _.template($("#"+container).html());
+		
+		var obj = {};
+		obj[key] = data;
 
+		var html = compiled(obj);
+		$("."+container).html(html);
+	}
+
+	function updateEvents(alarms) {
 		var events = _(alarms).sortBy("occurrence").reverse();
-		var eventHTML = eventCompiled({"alarms": events});
+		update("events", "alarms", events);
+	}
 
-		$(".events").html(eventHTML);
-
-		var legendCompiled = _.template($("#legend").html());
+	function updateLegend(alarms) {
 		var legends = _.chain(alarms).groupBy("proiority").map(function(value, key) {
 			return {proiority: key, count: value.length};
 		}).value();
 
-		var legendHTML = legendCompiled({"legends": legends});
-		$(".legend").html(legendHTML);
+		update("legend", "legends", legends);
+	}
+
+	$.get("/alarms.json").done(function(alarms) {
+		updateEvents(alarms);
+		updateLegend(alarms);
 	});
 });
